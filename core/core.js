eda6e6c888ce583097ae402b7f42e9307ab9f689
wukangjun <838927844@qq.ocm>
2018-07-03 20:40:10 +0800

    first commit

--- core/core.js
+++ core/core.js
@@ -1,70 +1,249 @@
 (function(global, factory) {
-	typeof exprots === 'object' && typeof module !== 'undefined'
+	typeof exports === 'object' && typeof module !== 'undefined'
 	? module.exports = factory()
-	: typeof define === 'function' && denfine.amd ? define(factory) : global.FristVersion = factory();
+	: typeof define === 'function' && define.amd ? define(factory) : global.Cvtsp = factory();
 })(this, function() {
+
+	var create = Object.create || function(superClass) {
+		function O() {};
+		O.prototype = superClass.prototype;
+		return new O();
+	}
+
+	// 类（class）继承 原形链的继承
+	// subClass 子类
+	// superClass 父类
 	var inherits = function(subClass, superClass) {
-		subClass.prototype = Object.create(superClass.prototype, {
-			constructor: {
-				value: subClass,
-				configurable: true
-			}
-		});
+		subClass.prototype = create(superClass.prototype);
+		subClass.prototype.constructor = subClass;
 	}
-	
+
 	var possibleConstructorReturn = function(self, parent) {
-		return typeof parent === 'function' && parent ? parent : self;
+		return parent && typeof parent === 'function' ? parent : self;
 	}
 	
-	var isEl = function(el) {
-		return el && el.nodeType === 1;
-	}
-	
-	var placeholderRegexp = /\s*{{([^{}]+)}}\s*/g;
-	// <aaaaaa >
-	var nodeNameRegexp = /\s*<([^\s/+={}]+)/;
-	
-	function FristVersion() {
-		this.binding = {};
-		this.data = {};
+	// dom操作的基础方法
+	function isTextNode(el) {
+		return el && el.nodeType === 3;
+	} 
+
+	function findChilds(el, callback) {
+		var childs = el.children;
 		
-		/**
-		 * @return {HTML} 返回html片段
-		 */
-		this.parserHTML = function() {
-			return this.render();
+		for(var i = 0, child; child = childs[i++];) {
+			callback(child);
+			findChilds(child, callback);
 		}
-	};
-	
-	
-	FristVersion.prototype.observer = function(data) {
-		
 	}
-	
+
+	function getAttributes(el) {
+		var obj = {};
+		var attrs = el.attributes;
+
+		for(var i = 0, attr; attr = attrs[i++];) {
+			var name = attr.name;
+			var value = attr.value;
+			obj[name] = value;
+		}
+
+		return obj;
+	}
+
+	var componentRegexp = /\s*<([^\s]+)/;
+	var isEventRegexp = /\s*@/;
+	var isPropertyRegexp = /\s*:/;
+	var isExistRegexp = /[^{}]*({{([^{}]+)}})[^{}]*/g;
+	var getTextInRegexp = /[^{}]*{{([^{}]+)}}/;
+	var zimuRegexp = /\s*([\w]+)\s*/g;
+
 	/**
-	 * 渲染组件
-	 * @param {String} nodeName: 节点名称
-	 * @param {Object} el
+	 * 2. 监听类
+	 * 元素的属性赋值,  Component属性赋值
+	 * @param {Element| Component} el
+	 * @param {Constructor} context: 上下文环境
+	 * @param {String}: 属性
+	 * @param {String}: 表达式
+	 * el[attr] = exp;
+	 */
+	function Watcher(el, context, attr, exp) {
+		this.el = el;
+		this.context = context;
+		this.attr = attr;
+		this.exp = exp;
+
+		this.update();
+	}
+
+	Watcher.prototype.update = function() {
+		var keys = Object.keys(this.context.data);
+		var values = keys.map(function(key) {
+			return this.context.data[key];
+		}.bind(this))
+
+		var func = Function.apply(this, keys.concat('return ' + this.exp));
+
+		this.el[this.attr] = func.apply(this, values);
+	}
+
+	/**
+	 * 
+	 * @param {Stirng} str: 'divj,html,body,h1 h2 b ' // 100
+	 * @param {Boolean} exp:true 大小
+	 * @return {Function}
+	 */
+	function makeMap(str, exp) {
+		var obj = {};
+		var lists = str.split(',');   //[div, html]
+
+		for(var i = 0, list; list = lists[i++];) {
+			obj[list] = true;
+		}
+		return exp
+		? function(val) {return obj[val.toLowerCase()]}
+		: function(val) { return obj[val]}
+	}
+
+	var isHtmlTag = makeMap('div,html,body,h1,h2,b,a,header', true);
+
+	// 1. 核心类
+	function Cvtsp() {
+		this.parent = null;
+		this.children = [];
+		this.data = {};
+		// 存储属性监听的队列
+		this._binding = {};
+	}
+
+	Cvtsp.prototype.init = function() {
+		// 1. 数据双向绑定
+		this.observer(this.data);
+
+		// 2. 模版解析
+		this.parserHTML();
+	}
+
+	Cvtsp.prototype.observer = function(data) {
+		Object.keys(data).forEach(function(key) {
+			var value = data[key];
+
+			// important init
+			this._binding[key] = {
+				watcher: []    //[Watcher, Watcher, Watcher, ...] 放所有操作了当前属性{key}的关联 dom操作,属性赋值
+			};
+			var _binding = this._binding[key];
+			Object.defineProperty(data, key, {
+				configurable: true,
+				get: function() {
+					return value;
+				},
+				set: function(newVal) {
+					if(value === newVal) return;
+					// 数据发生变化了
+					_binding.watcher.forEach(function(watch) {
+						watch.update(); // dom内容实时更新
+					})
+				}
+			})
+		}.bind(this))
+	}
+
+	Cvtsp.prototype.parserHTML = function() {
+		var _this = this;
+		var template = this.template();
+		this.parent.innerHTML = template;
+
+		// 获取每个dom ==> 获取dom上的属性
+		// 1. 获取所有的子节点
+		findChilds(this.parent, function(child) {
+			var attrs = getAttributes(child);
+			
+			// 2. 区分标准html元素 还是组件元素
+			if(isHtmlTag(child.tagName)) {
+				// 3. 区分key是否为事件还是属性
+				Object.keys(attrs).forEach(function(attr) {
+					// 是否为事件类型
+					if(isEventRegexp.test(attr)) {
+						//console.log(attr) @click  attrs[attr] child
+						child.addEventListener(attr.replace(isEventRegexp, ''), function(event) {
+							var event = event || window.event;
+							var eventName = attrs[attr];
+
+							_this[eventName].call(_this, event); 
+						}, false);
+					}
+
+					// 是否为动态属性的
+				})
+			}
+
+			// 我们只要text节点
+			parserTextNode(child);
+			
+		});
+
+		function parserTextNode(child) {
+			var childNodes = child.childNodes;
+			for(var i = 0,child; child = childNodes[i++];) {
+				if(isTextNode(child)) {
+					var turnText = child.textContent;
+					var placeholders = turnText.match(isExistRegexp);  //所有的花括号分割
+
+					if(placeholders && placeholders.length) {
+						placeholders.forEach(function(p) {
+							// 1. 获取花括号中的表达式
+							var exp = p.match(getTextInRegexp)[1];
+							
+							// 2. 
+							var everyKeyLists = exp.match(zimuRegexp);
+							
+							everyKeyLists.forEach(function(key) {
+								//el, context, attr, exp
+								_this._binding[key].watcher.push(new Watcher(
+									child,
+									_this,
+									'textContent',  //value
+									exp
+								));
+							})
+						})
+					}
+				}
+			}
+
+		}
+	}	
+
+
+	/**
+	 * 渲染节点
+	 * @param {String|Element} nodeName 
+	 * @param {Element} el 
 	 */
 	function render(nodeName, el) {
-		if(!isEl(el)) return;
-		var name = nodeName.match(nodeNameRegexp);
-		var _nodeName = name ? name[1]: nodeName.localName;
-		var Component = new FristVersion._components[_nodeName]();
-		
-		el.innerHTML = Component.parserHTML();
+		// 1. 获取nodeName名字
+		var name = nodeName.match(componentRegexp); // app
+		var nodeName = typeof nodeName === 'string' ? name[1] : nodeName.tagName;
+		var Component = Cvtsp._components[nodeName.toLowerCase()];
+
+		return Component && new Component(el);
 	}
-	
+
+	/**
+	 * 注册全局组件
+	 * @param {String} name: 组件的名称
+	 * @param {Function} component: 构造函数
+	 */
 	function registerComponent(name, component) {
-		if(!FristVersion._components) {
-			FristVersion._components = {};
+		if(!Cvtsp._components) {
+			Cvtsp._components = {};
 		}
-		FristVersion._components[name] = component;
+		Cvtsp._components[name.toLowerCase()] = component;
 	}
-	
-	FristVersion.render = render;
-	FristVersion.inherits = inherits;
-	FristVersion.registerComponent = registerComponent;
-	FristVersion.possibleConstructorReturn = possibleConstructorReturn;
-	return FristVersion;
+
+	Cvtsp.render = render;
+	Cvtsp.inherits = inherits;
+	Cvtsp.registerComponent = registerComponent;
+	Cvtsp.possibleConstructorReturn = possibleConstructorReturn;
+
+	return Cvtsp;
 });
